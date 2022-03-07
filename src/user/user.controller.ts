ef241d60de2ea6b9d134d9e420fbcf956820c1b5
vedatkokmen <vedatkoekmen@gmail.com>
2022-03-07 21:22:52 +0300

    add email validation

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -8,16 +8,16 @@ import {
   Delete,
 } from '@nestjs/common';
 import { UserService } from './user.service';
-import { CreateUserDto } from './dto/create-user.dto';
-import { UpdateUserDto } from './dto/update-user.dto';
+import { CreateUserDto } from './dto';
+import { UpdateUserDto } from './dto';
 
 @Controller('users')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
   @Post()
-  create(@Body() createUserDto: CreateUserDto) {
-    return this.userService.create(createUserDto);
+  async create(@Body() createUserDto: CreateUserDto) {
+    return await this.userService.create(createUserDto);
   }
 
   @Get()
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { ForbiddenException, Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
@@ -7,8 +7,26 @@ import { UpdateUserDto } from './dto/update-user.dto';
 export class UserService {
   constructor(private prisma: PrismaService) {}
 
-  create(createUserDto: CreateUserDto) {
-    return this.prisma.user.create({ data: createUserDto });
+  async create(createUserDto: CreateUserDto) {
+    // TODO: Validate email
+    try {
+      if (
+        await this.prisma.user.findFirst({
+          where: { email: createUserDto.email },
+        })
+      ) {
+        throw new ForbiddenException('Credentials taken');
+      }
+      const user = await this.prisma.user.create({
+        data: {
+          ...createUserDto,
+          email: createUserDto.email,
+        },
+      });
+      return user;
+    } catch (error) {
+      throw error;
+    }
   }
 
   findAll() {
