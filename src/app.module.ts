0bcf5ceffbf7bd3992a12d55df31c7bad0ca63bf
vedatkokmen <vedatkoekmen@gmail.com>
2022-03-07 20:33:06 +0300

    add user crud func

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
+import { UserModule } from './user/user.module';
 
 @Module({
   imports: [
@@ -10,6 +11,7 @@ import { ConfigModule } from '@nestjs/config';
     ConfigModule.forRoot({
       isGlobal: true,
     }),
+    UserModule,
   ],
   controllers: [AppController],
   providers: [AppService],
