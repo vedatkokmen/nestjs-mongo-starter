548081037b20074df8f46cbf030279af15febd40
vedatkokmen <vedatkoekmen@gmail.com>
2022-03-07 19:58:50 +0300

    add mongoDB

---
+++ src/app.module.ts
@@ -0,0 +1,17 @@
+import { PrismaModule } from './prisma/prisma.module';
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+
+@Module({
+  imports: [
+    PrismaModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
